e897497f52facf8cfbc5bb54b91c2d7142e08c87
UTTAM2311 <2311uttam@gmail.com>
2016-07-05 18:06:23 +0530

    normalized data

--- src/main/java/com/learn/ml/classification/MLLinearRegression.java
+++ src/main/java/com/learn/ml/classification/MLLinearRegression.java
@@ -29,25 +29,25 @@ public class MLLinearRegression {
                 String[] parts = line.split(",");
                 double[] v = new double[parts.length - 1];
 
-                //normalized the data
+                // normalized the data
                 for (int i = 0; i < parts.length - 1; i++)
-                    v[i] = Double.parseDouble(parts[i])/1000;
+                    v[i] = Double.parseDouble(parts[i]) / 1000;
 
-                double yvalue = Double.parseDouble(parts[parts.length - 1])/1000;
+                double yvalue = Double.parseDouble(parts[parts.length - 1]) / 1000;
                 return new LabeledPoint(yvalue, Vectors.dense(v));
             }
         });
 
         System.out.println(parsedData.count());
 
-        //building a model
+        // building a model
         int numIterations = 30000;
         LinearRegressionWithSGD algorithm = new LinearRegressionWithSGD();
         algorithm.setIntercept(true);
         algorithm.optimizer().setStepSize(0.00005).setNumIterations(numIterations);
 
         final LinearRegressionModel model = algorithm.run(JavaRDD.toRDD(parsedData));
-    
+
         // Evaluate model on training examples and compute training error
         JavaRDD<Tuple2<Double, Double>> valuesAndPreds =
                 parsedData.map(new Function<LabeledPoint, Tuple2<Double, Double>>() {
@@ -57,21 +57,25 @@ public class MLLinearRegression {
                         return new Tuple2<Double, Double>(prediction, lab);
                     }
                 });
+
         double MSE = new JavaDoubleRDD(valuesAndPreds.map(new Function<Tuple2<Double, Double>, Object>() {
             public Object call(Tuple2<Double, Double> pair) {
                 return Math.pow(pair._1() - pair._2(), 2.0);
             }
         }).rdd()).mean();
-        System.out.println("training Root Mean Squared Error = " + Math.sqrt(MSE));
-
-        System.out.println(model.weights());
-        System.out.println(model.intercept());
 
         List<LabeledPoint> dataPoints = parsedData.collect();
         for (LabeledPoint point : dataPoints) {
             System.out.println("DataPoint: " + point.features() + " Actual Value: " + point.label() + " Expected Value:"
                     + model.predict(point.features()));
         }
+
+        System.out.println("training Root Mean Squared Error = " + Math.sqrt(MSE));
+
+        System.out.println(model.weights());
+        System.out.println(model.intercept());
+
+
     }
 }
 
